92a7eb63847b8b159970db4b4b63a884717ac8a0
Jorge <srcoke98@gmail.com>
2025-04-02 16:44:22 +0200

    Class Book created

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 const library = [];
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.id = crypto.randomUUID();
-    this. info = function() {
-       return "id: " + this.id + "" + this.title +" by " + this.author + ", " + this.pages + " pages, " + this.read + ".";
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+        this.id = crypto.randomUUID();
     }
 }
 
